dce89ebc5b2b03f778971c169f54923a0f414437
Anjula Perera <anjulaperera98@gmail.com>
2022-12-29 20:39:54 +0530

    next milestone: add camera function

--- lib/Screens/CreateGroup.dart
+++ lib/Screens/CreateGroup.dart
@@ -1,4 +1,5 @@
-import 'package:chat_dev_stack/CustomUI/ButtonCard.dart';
+import 'package:chat_dev_stack/CustomUI/AvatarCard.dart';
+// import 'package:chat_dev_stack/CustomUI/ButtonCard.dart';
 import 'package:chat_dev_stack/CustomUI/ContactCard.dart';
 import 'package:chat_dev_stack/Model/ChatModel.dart';
 import 'package:flutter/material.dart';
@@ -12,6 +13,21 @@ class CreateGroup extends StatefulWidget {
 
 class _CreateGroupState extends State<CreateGroup> {
   List<ChatModel> contacts = [
+    ChatModel(name: 'Anjula', about: 'Pissottek'),
+    ChatModel(name: 'Perera', about: 'Gammac'),
+    ChatModel(name: 'Junuju', about: 'elama ela'),
+    ChatModel(name: 'Anjula', about: 'Pissottek'),
+    ChatModel(name: 'Perera', about: 'Gammac'),
+    ChatModel(name: 'Junuju', about: 'elama ela'),
+    ChatModel(name: 'Anjula', about: 'Pissottek'),
+    ChatModel(name: 'Perera', about: 'Gammac'),
+    ChatModel(name: 'Junuju', about: 'elama ela'),
+    ChatModel(name: 'Anjula', about: 'Pissottek'),
+    ChatModel(name: 'Perera', about: 'Gammac'),
+    ChatModel(name: 'Junuju', about: 'elama ela'),
+    ChatModel(name: 'Anjula', about: 'Pissottek'),
+    ChatModel(name: 'Perera', about: 'Gammac'),
+    ChatModel(name: 'Junuju', about: 'elama ela'),
     ChatModel(name: 'Anjula', about: 'Pissottek'),
     ChatModel(name: 'Perera', about: 'Gammac'),
     ChatModel(name: 'Junuju', about: 'elama ela'),
@@ -51,26 +67,69 @@ class _CreateGroupState extends State<CreateGroup> {
           ),
         ],
       ),
-      body: ListView.builder(
-        itemCount: contacts.length,
-        itemBuilder: (context, index) {
-          return InkWell(
-            onTap: () {
-              if (contacts[index].select == false) {
-                setState(() {
-                  contacts[index].select = true;
-                  groups.add(contacts[index]);
-                });
-              } else {
-                setState(() {
-                  contacts[index].select = false;
-                  groups.remove(contacts[index]);
-                });
+      body: Stack(
+        children: [
+          ListView.builder(
+            itemCount: contacts.length + 1,
+            itemBuilder: (context, index) {
+              if (index == 0) {
+                return Container(
+                  height: groups.isNotEmpty ? 90 : 10,
+                );
               }
+              return InkWell(
+                onTap: () {
+                  if (contacts[index].select == false) {
+                    setState(() {
+                      contacts[index].select = true;
+                      groups.add(contacts[index]);
+                    });
+                  } else {
+                    setState(() {
+                      contacts[index].select = false;
+                      groups.remove(contacts[index]);
+                    });
+                  }
+                },
+                child: ContactCard(contact: contacts[index]),
+              );
             },
-            child: ContactCard(contact: contacts[index]),
-          );
-        },
+          ),
+          groups.isNotEmpty
+              ? Column(
+                  children: [
+                    Container(
+                      height: 75,
+                      color: Colors.white,
+                      child: ListView.builder(
+                        itemCount: contacts.length,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) {
+                          if (contacts[index].select == true) {
+                            return InkWell(
+                              onTap: () {
+                                setState(() {
+                                  contacts[index].select = false;
+                                  groups.remove(contacts[index]);
+                                });
+                              },
+                              child: AvatarCard(
+                                contact: contacts[index],
+                              ),
+                            );
+                          } else {
+                            return Container();
+                          }
+                        },
+                      ),
+                    ),
+                    const Divider(
+                      thickness: 1,
+                    ),
+                  ],
+                )
+              : Container(),
+        ],
       ),
     );
   }
